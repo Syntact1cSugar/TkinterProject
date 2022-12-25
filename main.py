1e2ccef5df69a94436c80dfc513574323f73a07f
Syntact1cSugar <121145814+Syntact1cSugar@users.noreply.github.com>
2022-12-25 18:15:24 +0530

    Update main.py
    Added following button on the root window : About and Exit
                                         on the child window : Back

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import os
 from tkinter import *
 
-
 window = Tk()
 
 window.title("Run Python Script")
@@ -11,9 +10,25 @@ window.geometry('550x200')
 
 def run():
     os.system('list.py')
+    return
 
 
-btn = Button(window, text="Click To Run Doubly Linked List", bg="black", fg="white", command=run)
-btn.place(x=175, y=100)
+def createNewWindow():
+    newWindow = Toplevel(window)
+    newWindow.title("About")
+    newWindow.geometry('550x200')
+    text = Text(newWindow, width=50, height=30, background="white", foreground="black", font=('Times New Roman', 13, 'bold'))
+    text.insert(INSERT, "This code was contributed by : \n\n1.Subramanya H(20GACSE069) \n\n2.Suchit Priyadarshi(20GACSE070 \n\n3.Sujatha Bhat(20GACSE071)")
+    text.pack(expand=1, fill=BOTH)
+    btn = Button(newWindow, text="Back", bg="black", fg="white", command=newWindow.destroy)
+    btn.place(x=100, y=150)
+    return
 
+
+btn1 = Button(window, text="Click To Run Doubly Linked List", bg="black", fg="white", command=run)
+btn2 = Button(window, text="About", fg="white", bg="black", command=createNewWindow)
+btn3 = Button(window, text="Exit", bg="black", fg="white", command=window.destroy)
+btn1.place(x=175, y=50)
+btn2.place(x=240, y=100)
+btn3.place(x=245, y=150)
 window.mainloop()
