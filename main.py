6e8e106620b84dcb8b9878a2a7b34eb17b2d9f7d
Syntact1cSugar <121145814+Syntact1cSugar@users.noreply.github.com>
2022-12-25 16:21:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 import os
 from tkinter import *
 
+
 window = Tk()
 
-window.title("Running Python Script")
+window.title("Run Python Script")
 window.geometry('550x200')
 
 
@@ -12,7 +13,7 @@ def run():
     os.system('list.py')
 
 
-btn = Button(window, text="Click Me", bg="black", fg="white", command=run)
-btn.grid(column=0, row=0)
+btn = Button(window, text="Click To Run Doubly Linked List", bg="black", fg="white", command=run)
+btn.place(x=175, y=100)
 
 window.mainloop()
